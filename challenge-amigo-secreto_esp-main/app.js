e53fb6f1bcd1a3c0bc59ee70af27279d7971f4d4
Germán Franke <germanfranke@gmail.com>
2025-02-13 23:44:44 +0000

    working on function recorrerArray

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,16 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo(){
+function agregarAmigo(){    
     let elem = document.getElementById("amigo").value;
 
     if(elem == ""){
         alert("Por favor, inserte un nombre.")
     } else {
         amigos.push(elem);    
-    }
+    };
+
+    console.log(amigos);
     
     elem = document.getElementById("amigo").value="";
 
-    return document.getElementById('listaAmigos').textContent = amigos;
+    //Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. 
+    // Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+    function recorrerArray(array, lista){
+
+        //Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+        let listaAmigos = document.getElementById(lista);
+
+        //Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+        lista.innerHTML = "";
+
+        //Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+        for(let el = 0; array.lenght > 0; el++){
+            //Para cada amigo, crear un nuevo elemento de lista.
+            listaAmigos = document.createElement('li').value = el;
+        }
+
+        return 
+    };
+    
+    recorrerArray(amigos, 'listaAmigos');
+    return 
 }
+
